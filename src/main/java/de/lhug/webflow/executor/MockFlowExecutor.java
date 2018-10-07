c4a2d4c144ca5e9a038d71d6baac4341a5019d4a
Unknown <lhug@arcor.de>
2018-10-07 20:08:59 +0200

    MockFlowExecutor
    Extracted MockFlowExecutor
    CreatedFlow-Factory
    need to extract Flow Building to extra builder class or tool

---
+++ src/main/java/de/lhug/webflow/executor/MockFlowExecutor.java
@@ -0,0 +1,54 @@
+package de.lhug.webflow.executor;
+
+import org.springframework.webflow.context.ExternalContext;
+import org.springframework.webflow.core.collection.MutableAttributeMap;
+import org.springframework.webflow.definition.FlowDefinition;
+import org.springframework.webflow.engine.impl.FlowExecutionImpl;
+import org.springframework.webflow.engine.impl.FlowExecutionImplFactory;
+import org.springframework.webflow.execution.FlowExecution;
+import org.springframework.webflow.execution.FlowExecutionFactory;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class MockFlowExecutor {
+
+	private final FlowDefinition flowDefinition;
+	private final FlowExecutionFactory flowExecutionFactory = new FlowExecutionImplFactory();
+
+	@Getter
+	private FlowExecution flowExecution;
+
+	public void startFlow(ExternalContext externalContext) {
+		startFlow(null, externalContext);
+	}
+
+	public void startFlow(MutableAttributeMap parameters, ExternalContext externalContext) {
+		buildFlowExecution();
+		flowExecution.start(parameters, externalContext);
+	}
+
+	private void buildFlowExecution() {
+		flowExecution = flowExecutionFactory.createFlowExecution(flowDefinition);
+	}
+
+	public void resumeFlow(ExternalContext context) {
+		if (flowExecution == null) {
+			throw new IllegalStateException("Cannot resume flow before it was started!");
+		}
+		flowExecution.resume(context);
+	}
+
+	public void setCurrentState(String stateId) {
+		if (flowExecution == null) {
+			buildFlowExecution();
+		}
+		setState(stateId);
+	}
+
+	private void setState(String stateId) {
+		((FlowExecutionImpl) flowExecution).setCurrentState(stateId);
+	}
+
+}
